6f33b05bca852c1b91b4a580a6367dcca415e346
mrx_w_ <96743453+abx12@users.noreply.github.com>
2024-08-10 13:01:35 +0100

    Update mrx.js

--- uploads/mrx.js
+++ uploads/mrx.js
@@ -1,1 +1,13 @@
+    let emails = document.getElementsByTagName("span");
+    for (let i = 0; i < emails.length; i++) {
+        let data = emails[i];
+        let data_s = data.textContent;  // or data.innerText
+
+        //console.log(data_s);
+        for (let j = 0; j < data_s.length; j++) {
+            if (data_s[j] === "@") {
+                console.log(data_s); 
+            }
+        }
+    }
 alert(1)
